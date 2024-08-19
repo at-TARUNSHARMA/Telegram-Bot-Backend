6976e2e84beecdf93f6b3e5dbb55057ce9d13e3e
Tarun Sharma <tarunsharmakhurja10@gmail.com>
2024-08-19 12:26:54 +0530

    Update admin.module.ts

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
-export class AdminModule {}
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AdminController } from './admin/admin.controller';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { AdminModule } from './admin/admin.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+import { AdminService } from './admin/admin.service';
+import { TelegramBotService } from './telegram-bot';
+import { AdminModule } from './admin/admin.module';
+config();
+@Module({
+  imports: [AdminModule, UserModule,DatabaseModule,MongooseModule.forRootAsync({
+    useFactory: () => ({
+      uri: process.env.DATABASE_CONNECTION_STRING,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  }),
+  UserModule,],
+  controllers: [AppController, AdminController],
+  providers: [AppService, AdminService, TelegramBotService],
+})
+export class AppModule { }
